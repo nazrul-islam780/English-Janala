42b6aac157a4233ef004a8e854a74dd8795a8d11
mac <mac@Nazruls-MacBook-Pro.local>
2026-02-14 01:46:12 +0600

    added dynamic modal while clicking card info icon

--- scripts/scripts.js
+++ scripts/scripts.js
@@ -1,3 +1,9 @@
+const createElements = (arr) => {
+    const htmlElements = arr.map((el) => `<span class="btn border-[1px] border-[#D7E4EF] bg-[#EDF7FF] mr-4">${el}</span>`);
+    return (htmlElements.join(""))
+}
+
+
 const loadLessons = () => {
     fetch('https://openapi.programming-hero.com/api/levels/all')
         .then((res) => res.json())
@@ -6,10 +12,20 @@ const loadLessons = () => {
 
 }
 
-const removeActive = () =>{
+const removeActive = () => {
     const lessonButtons = document.querySelectorAll('.lesson-btn')
     lessonButtons.forEach(btn => btn.classList.remove('active'))
-    
+
+}
+
+const loadWordDetail = async(id) => {
+    const url = `https://openapi.programming-hero.com/api/word/${id}`
+    // fetch(url)
+        // .then(res => res.json())
+        // .then(data => console.log(data.data))
+    const res = await fetch(url);
+    const details = await res.json()
+    displayWordDetails(details.data)
 }
 
 // loadLessons()
@@ -26,8 +42,29 @@ const loadLevelWord = (id) => {
             displayLoadWord(data.data)
         });
 
-    
+
     // return url;
+}
+
+const displayWordDetails = word => {
+    console.log(word)
+    const detailsBox = document.getElementById('details-container');
+    detailsBox.innerHTML = `<div class="">
+                        <h2 class="text-4xl font-semibold">${word.word} (<i class="fa-solid fa-microphone"></i> : ${word.pronunciation})</h2>
+                    </div>
+                    <div class="">
+                        <h2 class="text-2xl font-semibold mb-2">Meaning</h2>
+                        <p class="font-bangla text-2xl font-medium">${word.meaning}</p>
+                    </div>
+                    <div class="">
+                        <h2 class="text-2xl font-semibold mb-2">Example</h2>
+                        <p class="text-2xl font-medium">${word.sentence}</p>
+                    </div>
+                    <div class="">
+                        <h2 class="text-2xl font-medium font-bangla mb-2">সমার্থক শব্দ গুলো</h2>
+                        <div class="">${createElements(word.synonyms)}</div>
+                    </div>`;
+    document.getElementById('word_modal').showModal();
 }
 
 const displayLoadWord = (words) => {
@@ -36,7 +73,7 @@ const displayLoadWord = (words) => {
     // cardContainer.innerHTML = '<h1>hello world</a>';    
     cardContainer.innerHTML = '';
 
-    if(words.length == 0){
+    if (words.length == 0) {
         cardContainer.innerHTML = `
         <div class="text-center col-span-full my-6">
 
@@ -54,16 +91,15 @@ const displayLoadWord = (words) => {
         cardDiv.innerHTML = `<div class="shadow-sm bg-white w-auto text-center py-10 px-7 rounded-xl">
                  <h2 class="text-3xl font-bold">${word.word ? word.word : "শব্দ পাওয়া যায়নি "}</h2>
                  <p class="my-6 font-medium text-[20px]">Meaning/ Pronounciation</p>
-                 <h2 class="text-[30px] font-medium text-[#18181B]">"${word.meaning ? word.meaning : "অর্থ পাওয়া যায়নি"}/ ${word.pronunciation? word.pronunciation : "Pronunciation পাওয়া যায়নি "}"</h2>
+                 <h2 class="text-[30px] font-medium text-[#18181B]">"${word.meaning ? word.meaning : "অর্থ পাওয়া যায়নি"}/ ${word.pronunciation ? word.pronunciation : "Pronunciation পাওয়া যায়নি "}"</h2>
                  <div class="flex justify-between mt-12">
-                    <button class="btn bg-[#1A91FF1A] text-xl"><i class="fa-solid fa-circle-info"></i></button>
+                    <button class="btn bg-[#1A91FF1A] text-xl" onclick="loadWordDetail(${word.id})"><i class="fa-solid fa-circle-info"></i></button>
                     <button class="btn bg-[#1A91FF1A] text-xl"><i class="fa-solid fa-volume-high"></i></button>
                  </div>
              </div>`;
         cardContainer.append(cardDiv)
     }
 }
-
 
 const displayLessons = (lessons) => {
     // console.log(lesson)
